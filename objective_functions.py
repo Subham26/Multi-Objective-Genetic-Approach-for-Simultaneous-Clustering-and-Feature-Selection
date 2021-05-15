0d9d1ff1914af43f9db85c67f662716d3dd530f2
Subham Ghosh <36621264+Subham26@users.noreply.github.com>
2021-05-15 17:35:23 +0530

    Update objective_functions.py

--- objective_functions.py
+++ objective_functions.py
@@ -3,18 +3,14 @@ import mutual_info
 
 
 # Objective-I: Within-Cluster-Variance
-def within_cluster_variance(data, chromosome, minimum_feature_count):
+
+def within_cluster_variance(chromosome, data):
 
     # Number of clusters
     K = len(chromosome[1])
 
     # Number of data points
     instance_count = data.shape[0]
-
-    # Choromosome should select the minimum number of features
-
-    while chromosome[0].count(1) < minimum_feature_count:
-        chromosome[0][random.randint(0, len(chromosome[0]) - 1)] = 1
 
     # Distances between cluster medoids should be > 0
 
@@ -38,7 +34,9 @@ def within_cluster_variance(data, chromosome, minimum_feature_count):
 
   
 # Objective-II: Mutual Information-based Redundancy
+
 def compute_redundancy(feature_set, data):
+    
     # data [instance_count X feature_count]
     instance_count, feature_count = data.shape
     selected_feature_count = feature_set.count(1)
