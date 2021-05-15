6594ec26cf781975d7efe4d5bddf235280cb59ee
Subham Ghosh <36621264+Subham26@users.noreply.github.com>
2021-05-15 21:51:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ K = 4
 df = pd.read_csv("dataset\soybean-small.data", header=None)
 data = df.to_numpy()
 
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
 nsga2(data, K)
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
