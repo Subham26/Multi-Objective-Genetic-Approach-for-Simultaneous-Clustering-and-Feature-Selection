84aee7d943fc46e99d9aef4a1e56fd395e6ed90b
Subham Ghosh <36621264+Subham26@users.noreply.github.com>
2021-05-16 20:34:22 +0530

    Update cat_dist.py

--- cat_dist.py
+++ cat_dist.py
@@ -1,15 +1,12 @@
 # Computing distance between two data points
-def distance(data, index1, index2, feature_vector):
-    feature_count = len(feature_vector)
+def distance(point1, point2):
+    feature_count = len(point1)
 
     # "cat_dist" stands for distance metric for Categorical data
     cat_dist = 0
     
+    # Computing Hamming distance
     for counter in range(feature_count):
-        cat_dist += (data[index1][counter] != data[index2][counter]) \
-              * feature_vector[counter]
-    
-    # Distance normalization 
-    # cat_dist = cat_dist * 1.0 / float(feature_vector.count(1))
+        cat_dist += (point1[counter] != point2[counter])
 
     return cat_dist
